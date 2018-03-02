ababa680a147f63c801a361a7aa669cc130c3993
Flühmann Tobias, INI-ON-NTO-COY-NNI <tobias.fluehmann@swisscom.com>
2018-03-02 14:29:54 +0100

    create and delete added

--- app.rb
+++ app.rb
@@ -9,7 +9,9 @@ require 'db'
 require 'customer'
 
 class App < Sinatra::Base
+
   get '/' do
+    content_type :json
     "Hello from sinatra! The time is #{ Time.now.to_i } on #{ `hostname` }!"
   end
 
@@ -21,9 +23,25 @@ class App < Sinatra::Base
     {ok: "yes"}.to_json
   end
 
-  post '/customer/:customer_name' do
-    name = params[:customer_name]
+  put '/customer/:name' do
+    content_type :json
+    Customer.create(name: params[:name], last_name: params[:last_name], address: params[:address])
+    status 200
+    "customer created".to_json
+  rescue Sequel::UniqueConstraintViolation => e
+    puts e.message
+    status 409
+    "already exists"
+  end
 
+  delete '/customer/:name' do
+    content_type :json
+    Customer.where(name: params[:name]).first.destroy
+    status 200
+    "customer deleted".to_json
+  rescue
+    status 404
+    "customer not found"
   end
 end
 
